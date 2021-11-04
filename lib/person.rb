d7c6a345392b0cbaa5fa4495f6477600f913811e
Yeison <enmanuelquiterios@gmail.com>
2021-11-04 11:45:39 -0400

    Completed

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,9 @@
 class Person
-  # your code here
+  def def initialize(attributes)
+    attributes.each do |key,value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+
+  end
+
 end
